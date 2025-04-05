a11a76ea065ac3dd149cfa325deeb509b68e09cf
Raul Almeida <devraul7@gmail.com>
2025-04-05 15:20:52 -0300

    Alteração no controle do robo

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,8 +5,8 @@
 package frc.robot;
 
 import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
@@ -19,7 +19,7 @@ public class Robot extends TimedRobot {
   private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
   private final DifferentialDrive m_robotDrive =
       new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
-  private final Joystick m_stick = new Joystick(0);
+  private final XboxController m_stick = new XboxController(0);
 
   public Robot() {
     SendableRegistry.addChild(m_robotDrive, m_leftMotor);
@@ -40,6 +40,6 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(-m_stick.getY(), -m_stick.getX());
+    m_robotDrive.arcadeDrive(-m_stick.getRawAxis(4), -m_stick.getRawAxis(1));
   }
 }
